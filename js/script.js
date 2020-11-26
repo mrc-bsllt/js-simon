f1984ac8e85d0f580a4f7fa5c7b37d685c01fba1
MarcoB <mrc.bsllt@gmail.com>
2020-11-26 13:39:44 +0100

    creato un alert con 5 numeri casuali da 1 a 100

--- js/script.js
+++ js/script.js
@@ -1,4 +1,36 @@
 $(document).ready(
   function() {
+    // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+    // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+    // creo un Array con 5 numeri casuali diversi da inserire nel Prompt
+    var prova = [];
+    for (var i = 0; i < 5; i++) {
+      var element = randomNumber(1, 100);
+      if (isInArray(prova, element)) {
+        i--;
+      } else {
+        prova.push(element);
+      }
+    }
+
+    // creo un alert che mi dica i 5 numeri casuali dentro l'array prova
+    alert(prova.join(", "));
+
   }
 );
+
+// FUNZIONI---------------------------------------------------
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function isInArray(array, number) {
+  var found = false;
+  for (var i = 0; i < array.length; i++) {
+    if (number == array[i]) {
+      return found = true;
+    }
+  }
+  return found;
+}
